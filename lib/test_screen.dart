c1314ece81c6b268a3db58b03014ac198df600f2
runway <lotfimhamed22@gmail.com>
2026-06-20 12:46:18 +0300

    get & getAt & value

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: must_be_immutable
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -14,8 +15,8 @@ class TestScreen extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () async {
-                box = await Hive.openBox("test");
-                print("opened");
+                box = await Hive.openBox("testGet"); // open box
+                print("testGet is opened");
               },
               child: Text("Open Box"),
             ),
@@ -30,15 +31,18 @@ class TestScreen extends StatelessWidget {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                box.add(false);
+                // box.add("lotfi");
+                // box.add("Dod");
+                // print("lotfi is added");
               },
               child: Text("add data"),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // print(box.get("localization"));
-                print(box.get(7)); //true
+                print(box.get(0));
+                print(box.getAt(0));
+                print(box.values.toList());
               },
               child: Text("Display"),
             ),
